37e6d0bf8bb6e50eabc881c86788e91dadacfabe
jinu <wlsdn5153@naver.com>
2024-05-08 16:31:06 +0900

    로딩 방식 자동으로 변경

--- src/components/Loading/Loading2.tsx
+++ src/components/Loading/Loading2.tsx
@@ -1,10 +1,10 @@
-import { useIsAssetDownloaded } from "../../stores/LoadingStore";
+import { useIsAssetDownloaded, useLoadingActions } from "../../stores/LoadingStore";
 import { opacityBlurVariants } from "../../utils/variants";
-import LoadingButton from "./LoadingButton";
 import { motion } from "framer-motion";
 
 const Loading2 = () => {
   const isAssetDownloaded = useIsAssetDownloaded();
+  const handleLoading = useLoadingActions("handleLoading");
 
   return (
     <div className="flex flex-col items-center leading-[5.5vmax] font-black w-full text-sd-gray">
@@ -26,21 +26,15 @@ const Loading2 = () => {
         variants={opacityBlurVariants}
         initial="initial"
         animate={isAssetDownloaded ? "animate" : "initial"}
+        onAnimationComplete={() => {
+          handleLoading(false);
+        }}
         className="flex ml-[6vmax] text-[6vmax]"
       >
         <div>R</div>
         <div>O</div>
         <div>O</div>
         <div>M</div>
-      </motion.div>
-
-      <motion.div
-        transition={{ delay: 0.6, type: "tween", ease: "easeIn" }}
-        variants={opacityBlurVariants}
-        initial="initial"
-        animate={isAssetDownloaded ? "animate" : "initial"}
-      >
-        <LoadingButton />
       </motion.div>
     </div>
   );
